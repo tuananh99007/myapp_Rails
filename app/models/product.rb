7d4c561a92826aeb86a7f69863527769714b6ff2
tuananh99007 <tuananh99007@gmail.com>
2023-10-04 13:55:01 +0700

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :category
+
     def self.category_name
         category = @categories.find { |cat| cat.id == category_id }
         category.present? ? category.name : "Unknown Category"
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,8 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'categories/destroy'
 
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
-
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
+  resources :users
+  resources :categories
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
